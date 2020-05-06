7516b8af4b05317c2ec48c30ccb2e8cf4e9a3e20
4vllad <online-wllad@mail.ru>
2020-05-06 15:55:08 +0200

    Rework of Bitcon folder

---
+++ bitcon/effects.js
@@ -0,0 +1,29 @@
+
+function drawSpark(n,xx,yy,width, height) {
+    let spark = new Object();
+    let spark_image = new Image();
+    spark_image.src = "pictures/spark.png";
+
+    spark.x = xx;
+    spark.y = yy;
+    spark.width = width;
+    spark.height = height;
+    let sparks = new Array(n);
+
+    ctx.beginPath();
+    //ctx.fillStyle = "yellow";
+    //ctx.fillRect(spark.x, spark.y,spark.width,spark.height);
+    ctx.drawImage(spark_image,spark.x, spark.y,spark.width,spark.height);
+    //enemy_image = new Image();
+
+    //ctx.drawImage(enemy_image,ballX,ballY,ballWidth,ballHeight);
+    ctx.closePath();
+    timeForAction = time%2;
+    if (timeForAction == 1){
+        sparkLifeTime = 0;
+        sparkHit = false;
+        move = 0;
+    }
+
+}
+
