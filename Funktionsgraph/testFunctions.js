aca627b02fd30513cbf2b648a9779c856ce800cb
4vllad <online-wllad@mail.ru>
2021-01-08 17:43:41 +0100

    Test Funktionen Buttons

---
+++ Funktionsgraph/testFunctions.js
@@ -0,0 +1,30 @@
+function inputParable() {
+    document.getElementById("input").value = "x^2";
+    draw();
+}
+
+function inputHyperbel() {
+    document.getElementById("input").value = "x^3";
+    draw();
+}
+
+function inputHyperbelx4() {
+    document.getElementById("input").value = "x^4";
+    draw();
+}
+
+function inputHyperbelx5() {
+    document.getElementById("input").value = "x^5";
+    draw();
+}
+
+function inputHyperbelx6() {
+    document.getElementById("input").value = "x^6";
+    draw();
+}
+
+function inputHyperbelx7() {
+    document.getElementById("input").value = "x^7";
+    draw();
+}
+
