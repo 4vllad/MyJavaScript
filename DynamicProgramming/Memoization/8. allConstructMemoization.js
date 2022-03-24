dabbd9fd1d892c06c1c423e3ca69a590228e0b53
A <34423493+4vllad@users.noreply.github.com>
2022-03-24 21:24:17 +0100

    Update 8. allConstructMemoization.js

--- DynamicProgramming/Memoization/8. allConstructMemoization.js
+++ DynamicProgramming/Memoization/8. allConstructMemoization.js
@@ -1,8 +1,22 @@
-const allCounstruct = () => {
+const allConstruct = (target, wordBank, memo={}) => {
+    if (target in memo) return memo[target];
+    if (target === "") return [[]]
 
+    const result = [];
+
+    for (let word of wordBank){
+        if (target.indexOf(word) === 0){
+            const suffix = target.slice(word.length);
+            const suffixWays = allConstruct(suffix, wordBank, memo);
+            const targetWays = suffixWays.map(way => [word, ...way]);
+            result.push( ...targetWays);
+        }
+    }
+    memo[target] = result;
+    return result;
 }
 
-console.log(countConstruct("abc", ["ab", "bc", "ac", "c"]));
-console.log(countConstruct("abc", ["ab", "bc", "ac"]));
-console.log(countConstruct("ac", ["ab", "bc", "ac"]));
-console.log(countConstruct("abc", ["a","b", "c", "ab", "bc"]));
+console.log(allConstruct("abc", ["ab", "bc", "ac", "c"]));
+console.log(allConstruct("abc", ["ab", "bc", "ac"]));
+console.log(allConstruct("ac", ["ab", "bc", "ac"]));
+console.log(allConstruct("abc", ["a","b", "c", "ab", "bc"]));
