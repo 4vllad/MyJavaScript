a3bf41113afe195d06d18c02b1a08aaac9aab4bb
A <34423493+4vllad@users.noreply.github.com>
2022-10-26 22:26:54 +0200

    Fix Cheat Menu

--- Towns/cheats.js
+++ Towns/cheats.js
@@ -5,34 +5,57 @@ function addGold() {
     //document.getElementById("test2").innerHTML = goldinput;
     gold = gold + goldinput;
     document.getElementById("line21").innerHTML = "gold: " + gold;
+    playCoinAudio();
 }
 
 //More Population
 function cheatHeroHealth() {
-    heroHealth = heroHealth + 100;
-    document.getElementById("line71").innerHTML = " spearmen: " + spearmen;
+    clearCanvas();
+    heroHealth = heroHealth + 1000;
+    playerHealth = heroHealth;
+    drawStats();
+    resetCanvas();
 }
 
 //More Attack
 function cheatAttack() {
+    clearCanvas();
     playerPower = playerPower * 10;
     drawStats();
+    resetCanvas();
 }
 
 function visibleSaber() {
+    playerPower = 100000;// More OP
+    heroHealth = 100000;
+    playerHealth = heroHealth;
     playerModel = true;
     saberVisibility = true;
     console.log("visible");
     enemyModel = true;
     ratVisibility = true;
+    worldCtx.clearRect(0,0,920,1000);
+    drawStats();
+    resetCanvas();
+    
 }
 
+function clearCanvas() {
+    worldCtx.clearRect(0,0,920,1000);
+}
+
+function resetCanvas() {
+    drawCircle();
+    drawGround();
+    drawRightGuy(0); //Right Guy
+    drawLeftGuy(0) //Left Guy
+}
 
 
 //CheatsVisible
 var cheatClickCount = false;
 function cheatsVisible() {
-    cheatClickCount = false;
+    cheatClickCount = true;
     if (cheatClickCount == true) {
         document.getElementById("cheat").style.visibility = "visible";
     }
